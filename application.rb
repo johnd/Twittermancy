3ad6081660a464ad9e330092f7d9c66e5d7a4021
John Daniels <john@semantici.st>
2009-07-20 19:12:07 +0100

    Bare bones of app

--- application.rb
+++ application.rb
@@ -2,3 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'lib/twittermancy'
 
+get '/:search.json' do
+  words = Divination.new(params[:search])
+  words.results.to_json
+end
---
+++ lib/twittermancy.rb
@@ -0,0 +1,50 @@
+class Divination
+  require 'rubygems'
+  require 'activesupport'
+  require 'open-uri'
+  require 'json'
+  require 'digest/sha1'
+
+  def initialize(search_term, time_delay=2, cache_root='cache')
+
+    cache_file = cache_root + "/" + Digest::SHA1.hexdigest(search_term)
+
+    if File.exists?(cache_file) && File.mtime(cache_file) > time_delay.hours.ago
+      cache = File.new(cache_file, 'r')
+      @feed = JSON.parse(cache.gets)
+
+    else
+
+      source = "http://search.twitter.com/search.json?q=#{search_term}&rpp=50&lang=en"
+      content = ""
+      open(source) do |s| 
+        content = s.read
+      end
+
+      cache = File.new(cache_file, 'w+')
+      cache.puts content
+      cache.rewind
+      @feed = JSON.parse(cache.gets)
+
+    end
+  end
+
+  def results
+    results = []
+    query = @feed['query'].downcase
+    @feed['results'].each do | entry |
+      text = sanitise entry['text']
+      word = text.downcase.scan(/\w+/).-([query]).rand
+      results << {:word => word, :tweet => text, :user => entry['from_user']}
+    end
+    return results
+  end
+  
+  private
+  
+  def sanitise(string)
+    # This is really ugly.
+    converter = Iconv.new('ASCII//IGNORE//TRANSLIT', 'UTF-8') 
+    converter.iconv(string).unpack('U*').select{ |cp| cp < 127 && cp > 31}.pack('U*').gsub(/@\w+/,'').gsub(/&.*?;/, '').gsub(/(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix,'')
+  end
+end
