a8a4a11f140269f064cad81a1a42c2c6d07d9f1c
John Daniels <john@semantici.st>
2009-07-20 14:59:13 +0100

    App framework.

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+set :use_sudo, false
+set :application, "twitermancy"
+set :repository,  "ssh://code@fury.xybur.net/~/private/#{application}.git"
+set :deploy_to, "/home/john/deploy/#{application}"
+set :user, 'john'
+set :runner, user
+set :scm, :git
+set :branch, "master"
+set :domain, 'fury.xybur.net'
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+set :server_name, "twittermancy.urbanpaganism.info"
+depend :remote, :command, :gem
+
+# Allow ssh to use ssh keys
+set :ssh_options, { :forward_agent => true }
+
+deploy.task :restart do
+  # Restart Passenger
+  run "touch #{current_path}/tmp/restart.txt"
+end
+
+after :deploy, 'deploy:cleanup'
