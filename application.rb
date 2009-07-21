e907cb4d26ae4462549ba11bb330809b6c02489a
John Daniels <john@Macish.(none)>
2009-07-21 13:26:19 +0100

    Improved error handling.

--- application.rb
+++ application.rb
@@ -7,16 +7,20 @@ get '/' do
 end
 
 post '/' do
+  puts "PARAMS: #{params[:text]}"
   @query = params[:text]
-  @results = Divination.new(URI.escape(@query)).results
+  unless @query.blank?
+    @results = Divination.new(URI.escape(@query)).results
+    if @results.blank?
+      @error = "No results for that keyword!"
+    end
+  else
+    @error = "No keyword specified!"
+  end
   erb :index
 end
 
 get '/:search.json' do
   words = Divination.new(params[:search])
   words.results.to_json
-end
-
-error do
-  'Sorry there was a nasty error - ' + env['sinatra.error'].name
 end
